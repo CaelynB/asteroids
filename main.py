57f2fc5edfc196263f648a11109ef83c7b81ed51
CaelynB <caelynlballiet@gmail.com>
2025-07-01 17:37:14 -0500

    initialize basic pygame window and game loop

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import pygame
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    # initialize pygame
+    pygame.init()
+
+    # set a new GUI window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # main game loop
+    while True:
+        for event in pygame.event.get():
+            # if the user clicks the close button, exit the game
+            if event.type == pygame.QUIT:
+                return
+            
+        # fill the screen with black color
+        screen.fill("black")
+
+        # refresh the screen
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
